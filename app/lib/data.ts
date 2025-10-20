7c13b79dcd95d049e96c08f0896fc83ac9583a24
Oleksandr-Oliferuk <oleksandr.oliferuk@gmail.com>
2025-10-20 21:45:23 +0300

    add new components and create fetch data

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -43,9 +43,6 @@ export async function fetchLatestInvoices() {
 
 export async function fetchCardData() {
   try {
-    // You can probably combine these into a single SQL query
-    // However, we are intentionally splitting them to demonstrate
-    // how to initialize multiple queries in parallel with JS.
     const invoiceCountPromise = sql`SELECT COUNT(*) FROM invoices`;
     const customerCountPromise = sql`SELECT COUNT(*) FROM customers`;
     const invoiceStatusPromise = sql`SELECT
@@ -71,7 +68,6 @@ export async function fetchCardData() {
       totalPendingInvoices,
     };
   } catch (error) {
-    console.error('Database Error:', error);
     throw new Error('Failed to fetch card data.');
   }
 }
